0075ba4c92dfb6930932fb0173fae0f75fcafd88
Davi Ribeiro <davi.ribeiro@rumoon.com.br>
2025-04-26 11:19:13 -0400

    feature/ Implementado base/Atividade1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hello = hello;
-const world = 'world';
-function hello(who = world) {
-    return `Hello ${who}! `;
+var types_1 = require("skeleton/dist/types");
+var fs = require("fs");
+var world = 'world';
+function hello(who) {
+    if (who === void 0) { who = world; }
+    return "Hello ".concat(who, "! ");
 }
+var getProducts = function () {
+    try {
+        // Lê o arquivo JSON de forma síncrona
+        var fileContents = fs.readFileSync('base/products.json', 'utf-8');
+        // Parseia o conteúdo JSON
+        var products = JSON.parse(fileContents);
+        // Exibe os produtos no console
+        console.log(products);
+    }
+    catch (error) {
+        // Caso ocorra erro ao ler o arquivo
+        console.error('Erro ao ler o arquivo:', error);
+    }
+};
+getProducts();
+console.log(hello());
+var a = types_1.ProductCategory.Moda;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,57 @@
+import {Product, ProductCategory} from "skeleton/dist/types";
+import * as fs from 'fs';
+import axios from 'axios';
 
-import {ProductCategory} from "skeleton/dist/types";
+const getProducts = async (): Promise<Product[]> => {
+  try {
+    const fileContents = fs.readFileSync('../base/products.json', 'utf-8');
+    const products = JSON.parse(fileContents);
 
-const world = 'world';
+    // Exibe os produtos no console
+    // console.log(products);
+    console.log(products.length);
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+    return products;
+  } catch (error) {
+    console.error('Erro ao ler o arquivo:', error);
+    return [];
+  }
+};
+
+const getAllowedCategory = async (product: Product) => {
+
+  const response = await axios.get(`https://posdesweb.igormaldonado.com.br/api/allowedCategory?category=${product.category}`);
+
+  console.log(`${product.id} - ${response.data.allowed} - ${product.category}`);
+
+  return response.data.allowed;
+
 }
 
-console.log(hello());
+const discardNotAllowedProducts = async (products: Product[]) => {
+
+  const allowedProducts = [];
+  for (const product of products) {
+    if ( await getAllowedCategory(product) ) {
+      allowedProducts.push(product);
+    }else{
+      // console.log(`Discarded ${product.id}`);
+    }
+  }
+  return allowedProducts;
+}
+
+(async () => {
+  const products = await getProducts();
+  const allowedProducts = await discardNotAllowedProducts(products);
+
+  fs.writeFileSync('../base/processed.json', JSON.stringify(allowedProducts.map(product => ({
+    id: product.id,
+    name: product.name
+  })), null, 2));
+
+  // console.log(allowedProducts);
+  console.log(allowedProducts.length);
+})();
 
 const a: ProductCategory = ProductCategory.Moda;
